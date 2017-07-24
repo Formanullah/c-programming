ceff8f1295c2dee461ebbebcb74e5ff1ff230415
Formanullah <formanullahsajib@gmail.com>
2017-07-24 12:34:40 +0600

    Add files via upload

---
+++ uva 12250.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+#include<string.h>
+using namespace std;
+int main()
+{
+    string s;
+    int c=0;
+    while(getline(cin,s))
+    {
+        c++;
+        if(s=="#")
+        {
+            break;
+        }
+        else if(s=="HELLO")
+        {
+            cout<<"Case "<<c<<": ENGLISH"<<endl;
+        }
+        else if(s=="HOLA")
+        {
+            cout<<"Case "<<c<<": SPANISH"<<endl;
+        }
+        else if(s=="HALLO")
+        {
+            cout<<"Case "<<c<<": GERMAN"<<endl;
+        }
+        else if(s=="BONJOUR")
+        {
+            cout<<"Case "<<c<<": FRENCH"<<endl;
+        }
+        else if(s=="CIAO")
+        {
+            cout<<"Case "<<c<<": ITALIAN"<<endl;
+        }
+        else if(s=="ZDRAVSTVUJTE")
+        {
+            cout<<"Case "<<c<<": RUSSIAN"<<endl;
+        }
+        else
+            cout<<"Case "<<c<<": UNKNOWN"<<endl;
+
+
+    }
+}
+
